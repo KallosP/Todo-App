75435e28d396171f45e64cc4ac60158072c13599
KallosP <pkallos19@gmail.com>
2024-09-28 19:13:10 -0700

    test commit

--- Todo App/ContentView.swift
+++ Todo App/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }.padding()
+            
+            VStack {
+                Text("Testing")
+            }
+            
+            
         }
-        .padding()
     }
 }
 
