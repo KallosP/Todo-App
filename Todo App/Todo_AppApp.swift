990160e19ae2e64a43f6bd368cba4556beba7028
KallosP <pkallos19@gmail.com>
2024-09-28 18:59:28 -0700

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Peter Kallos on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
